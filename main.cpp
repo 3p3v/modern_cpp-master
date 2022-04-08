9b51bac8ca274477ffe52c4950ed90e0f1cd5ab8
AdamGolecki <adam@golecki.pl>
2022-04-09 01:56:15 +0200

    zad 4-8

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <string>
 #include <memory>
+#include <ctime>
 #include "Shape.hpp"
 #include "Rectangle.hpp"
 #include "Square.hpp"
@@ -10,24 +11,37 @@
 
 using namespace std;
 
-//typedef vector<shared_ptr<Shape>> Collection;
 using Collection = vector<shared_ptr<Shape>>;
 
-bool sortByArea(shared_ptr<Shape> first, shared_ptr<Shape> second)
+constexpr auto nth_fib(const int& n) 
+{
+    unsigned long int num1 = 0;
+    unsigned long int num2 = 1;
+
+    for(int i = 0; i < n; i++)
+    {
+        num2 += num1;
+        num1 = num2 - num1; 
+    }
+
+    return num1;
+}
+
+auto sortByArea(shared_ptr<Shape> first, shared_ptr<Shape> second)
 {
     if(first == nullptr || second == nullptr)
         return false;
     return (first->getArea() < second->getArea());
 }
 
-bool perimeterBiggerThan20(shared_ptr<Shape> s)
+auto perimeterBiggerThan20(shared_ptr<Shape> s)
 {
     if(s)
         return (s->getPerimeter() > 20);
     return false;
 }
 
-bool areaLessThan10(shared_ptr<Shape> s)
+auto areaLessThan10(shared_ptr<Shape> s)
 {
     if(s)
         return (s->getArea() < 10);
@@ -36,23 +50,23 @@ bool areaLessThan10(shared_ptr<Shape> s)
 
 void printCollectionElements(const Collection& collection)
 {
-    for(Collection::const_iterator it = collection.begin(); it != collection.end(); ++it)
-        if(*it)
-            (*it)->print();
+    for(const auto& it: collection)
+        if(it)
+            (it)->print();
 }
 
 void printAreas(const Collection& collection)
 {
-    for(vector<shared_ptr<Shape>>::const_iterator it = collection.begin(); it != collection.end(); ++it)
-        if(*it)
-            cout << (*it)->getArea() << std::endl;
+    for(const auto& it: collection)
+        if(it)
+            cout << (it)->getArea() << std::endl;
 }
 
 void findFirstShapeMatchingPredicate(const Collection& collection,
                                      bool (*predicate)(shared_ptr<Shape> s),
                                      std::string info)
 {
-    Collection::const_iterator iter = std::find_if(collection.begin(), collection.end(), predicate);
+    const auto iter = std::find_if(collection.begin(), collection.end(), predicate);
     if(*iter != nullptr)
     {
         cout << "First shape matching predicate: " << info << endl;
@@ -66,6 +80,9 @@ void findFirstShapeMatchingPredicate(const Collection& collection,
 
 int main()
 {
+    cout<<"N-ty wyraz ciagu:"<<endl;
+    cout<< nth_fib(45) <<endl;
+    
     Collection shapes;
     shapes.push_back(make_shared<Circle>(2.0));
     shapes.push_back(make_shared<Circle>(3.0));
